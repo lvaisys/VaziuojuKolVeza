4853adcedb5bad354d089aa331ca5cc6852bae15
lvaisys <lvaisys@gmail.com>
2015-05-26 22:09:20 +0300

    ghhggh

--- app/src/main/java/transport/psi/vu/mif/com/vezukolveza/MainActivity.java
+++ app/src/main/java/transport/psi/vu/mif/com/vezukolveza/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int a = 5;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
