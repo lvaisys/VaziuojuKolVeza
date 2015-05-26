4e807ed855a514bbc9c3f0954408f5e3b0d4269d
lvaisys <lvaisys@gmail.com>
2015-05-26 22:56:39 +0300

    naujas activity

---
+++ app/src/main/java/transport/psi/vu/mif/com/vezukolveza/Activities/TripActivityFragment.java
@@ -0,0 +1,24 @@
+package transport.psi.vu.mif.com.vezukolveza.Activities;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import transport.psi.vu.mif.com.vezukolveza.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TripActivityFragment extends Fragment {
+
+    public TripActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trip, container, false);
+    }
+}
---
+++ app/src/main/java/transport/psi/vu/mif/com/vezukolveza/TripManager/GPSManager.java
@@ -0,0 +1,8 @@
+package transport.psi.vu.mif.com.vezukolveza.TripManager;
+
+/**
+ * Created by Laurynas on 2015-05-26.
+ */
+public class GPSManager {
+
+}
