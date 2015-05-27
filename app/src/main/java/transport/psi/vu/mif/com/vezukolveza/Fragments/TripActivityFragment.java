28ee28e1de9b03988fa0e91aefe2c67d30979ea8
lvaisys <lvaisys@gmail.com>
2015-05-27 23:03:42 +0300

    kazkas kazkur kazkada

--- app/src/main/java/transport/psi/vu/mif/com/vezukolveza/Fragments/TripActivityFragment.java
+++ app/src/main/java/transport/psi/vu/mif/com/vezukolveza/Fragments/TripActivityFragment.java
@@ -2,23 +2,81 @@ package transport.psi.vu.mif.com.vezukolveza.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Chronometer.OnChronometerTickListener;
+import android.widget.TextView;
 
+import transport.psi.vu.mif.com.vezukolveza.Activities.TripActivity;
 import transport.psi.vu.mif.com.vezukolveza.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class TripActivityFragment extends Fragment {
+public class TripActivityFragment extends Fragment implements View.OnClickListener{
 
     public TripActivityFragment() {
     }
+    Button endTripBtn;
+    Chronometer timeElapsed;
+    TextView tvElapsed;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trip, container, false);
+        View rootView =inflater.inflate(R.layout.fragment_trip, container, false);
+        endTripBtn = (Button) rootView.findViewById(R.id.end_trip_button);
+        endTripBtn.setOnClickListener(this);
+        tvElapsed = (TextView)rootView.findViewById(R.id.textViewElapsed);
+
+
+        timeElapsed  = (Chronometer) rootView.findViewById(R.id.chronometer);
+        timeElapsed.setOnChronometerTickListener(new OnChronometerTickListener(){
+            @Override
+            public void onChronometerTick(Chronometer cArg) {
+                long time = SystemClock.elapsedRealtime() - cArg.getBase();
+                int h   = (int)(time /3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s= (int)(time - h*3600000- m*60000)/1000 ;
+                String hh = h < 10 ? "0"+h: h+"";
+                String mm = m < 10 ? "0"+m: m+"";
+                String ss = s < 10 ? "0"+s: s+"";
+                cArg.setText(hh+":"+mm+":"+ss);
+            }
+        });
+        timeElapsed.setBase(SystemClock.elapsedRealtime());
+        timeElapsed.start();
+
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.end_trip_button:
+                timeElapsed.stop();
+                try
+                {
+                    long elapsedMillis = SystemClock.elapsedRealtime()
+                            - timeElapsed.getBase();
+                    tvElapsed.setText( (int)elapsedMillis);
+                }
+                catch (Exception ex){
+
+                }
+
+
+
+
+                TripActivity tripActivity = (TripActivity) getActivity();
+                tripActivity.openTripInfo();
+                break;
+        }
     }
 }
--- app/src/main/java/transport/psi/vu/mif/com/vezukolveza/Fragments/TripInfoFragment.java
+++ app/src/main/java/transport/psi/vu/mif/com/vezukolveza/Fragments/TripInfoFragment.java
@@ -7,38 +7,23 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import transport.psi.vu.mif.com.vezukolveza.Activities.TripActivity;
 import transport.psi.vu.mif.com.vezukolveza.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TripInfoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TripInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TripInfoFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+public class TripInfoFragment extends Fragment implements View.OnClickListener {
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button backBtn;
+
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TripInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static TripInfoFragment newInstance() {
         TripInfoFragment fragment = new TripInfoFragment();
         Bundle args = new Bundle();
@@ -48,23 +33,24 @@ public class TripInfoFragment extends Fragment {
     }
 
     public TripInfoFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_info, container, false);
+         View rootView =inflater.inflate(R.layout.fragment_trip_info, container, false);
+        backBtn = (Button)rootView.findViewById(R.id.backButton);
+        backBtn.setOnClickListener(this);
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -75,32 +61,12 @@ public class TripInfoFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onClick(View v) {
+        TripActivity tripActivity = (TripActivity) getActivity();
+        tripActivity.openMainActivity();
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
